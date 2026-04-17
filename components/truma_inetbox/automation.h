f5043b60fd9f947488c06a832d86e7c26eeeb44a
darrenjackson72 <darren@mylittleoffice.co.uk>
2026-04-17 17:15:33 +0100

    Change u_int types to standard uint types in automation.h

--- components/truma_inetbox/automation.h
+++ components/truma_inetbox/automation.h
@@ -8,18 +8,19 @@ namespace truma_inetbox {
 
 template<typename... Ts> class HeaterRoomTempAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
  public:
-  TEMPLATABLE_VALUE(u_int8_t, temperature)
-  TEMPLATABLE_VALUE(HeatingMode, heating_mode)
+  TEMPLATABLE_VALUE(uint8_t, temperature)
+  TEMPLATABLE_VALUE(uint8_t, heating_mode)   // was HeatingMode
 
   void play(Ts... x) override {
-    this->parent_->get_heater()->action_heater_room(this->temperature_.value_or(x..., 0),
-                                                    this->heating_mode_.value_or(x..., HeatingMode::HEATING_MODE_OFF));
+    this->parent_->get_heater()->action_heater_room(
+        this->temperature_.value_or(x..., 0),
+        static_cast<HeatingMode>(this->heating_mode_.value_or(x..., (uint8_t) HeatingMode::HEATING_MODE_OFF)));
   }
 };
 
 template<typename... Ts> class HeaterWaterTempAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
  public:
-  TEMPLATABLE_VALUE(u_int8_t, temperature)
+  TEMPLATABLE_VALUE(uint8_t, temperature)
 
   void play(Ts... x) override {
     this->parent_->get_heater()->action_heater_water(this->temperature_.value_or(x..., 0));
@@ -28,16 +29,17 @@ template<typename... Ts> class HeaterWaterTempAction : public Action<Ts...>, pub
 
 template<typename... Ts> class HeaterWaterTempEnumAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
  public:
-  TEMPLATABLE_VALUE(TargetTemp, temperature)
+  TEMPLATABLE_VALUE(uint16_t, temperature)   // was TargetTemp
 
   void play(Ts... x) override {
-    this->parent_->get_heater()->action_heater_water(this->temperature_.value_or(x..., TargetTemp::TARGET_TEMP_OFF));
+    this->parent_->get_heater()->action_heater_water(
+        static_cast<TargetTemp>(this->temperature_.value_or(x..., (uint16_t) TargetTemp::TARGET_TEMP_OFF)));
   }
 };
 
 template<typename... Ts> class HeaterElecPowerLevelAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
  public:
-  TEMPLATABLE_VALUE(u_int16_t, watt)
+  TEMPLATABLE_VALUE(uint16_t, watt)
 
   void play(Ts... x) override {
     this->parent_->get_heater()->action_heater_electric_power_level(this->watt_.value_or(x..., 0));
@@ -46,19 +48,19 @@ template<typename... Ts> class HeaterElecPowerLevelAction : public Action<Ts...>
 
 template<typename... Ts> class HeaterEnergyMixAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
  public:
-  TEMPLATABLE_VALUE(EnergyMix, energy_mix)
-  TEMPLATABLE_VALUE(ElectricPowerLevel, watt)
+  TEMPLATABLE_VALUE(uint8_t, energy_mix)     // was EnergyMix
+  TEMPLATABLE_VALUE(uint16_t, watt)          // was ElectricPowerLevel
 
   void play(Ts... x) override {
     this->parent_->get_heater()->action_heater_energy_mix(
-        this->energy_mix_.value_or(x..., EnergyMix::ENERGY_MIX_GAS),
-        this->watt_.value_or(x..., ElectricPowerLevel::ELECTRIC_POWER_LEVEL_0));
+        static_cast<EnergyMix>(this->energy_mix_.value_or(x..., (uint8_t) EnergyMix::ENERGY_MIX_GAS)),
+        static_cast<ElectricPowerLevel>(this->watt_.value_or(x..., (uint16_t) ElectricPowerLevel::ELECTRIC_POWER_LEVEL_0)));
   }
 };
 
 template<typename... Ts> class AirconManualTempAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
  public:
-  TEMPLATABLE_VALUE(u_int8_t, temperature)
+  TEMPLATABLE_VALUE(uint8_t, temperature)
 
   void play(Ts... x) override {
     this->parent_->get_aircon_manual()->action_set_temp(this->temperature_.value_or(x..., 0));
@@ -72,20 +74,23 @@ template<typename... Ts> class TimerDisableAction : public Action<Ts...>, public
 
 template<typename... Ts> class TimerActivateAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
  public:
-  TEMPLATABLE_VALUE(u_int16_t, start)
-  TEMPLATABLE_VALUE(u_int16_t, stop)
-  TEMPLATABLE_VALUE(u_int8_t, room_temperature)
-  TEMPLATABLE_VALUE(HeatingMode, heating_mode)
-  TEMPLATABLE_VALUE(u_int8_t, water_temperature)
-  TEMPLATABLE_VALUE(EnergyMix, energy_mix)
-  TEMPLATABLE_VALUE(ElectricPowerLevel, watt)
+  TEMPLATABLE_VALUE(uint16_t, start)
+  TEMPLATABLE_VALUE(uint16_t, stop)
+  TEMPLATABLE_VALUE(uint8_t, room_temperature)
+  TEMPLATABLE_VALUE(uint8_t, heating_mode)   // was HeatingMode
+  TEMPLATABLE_VALUE(uint8_t, water_temperature)
+  TEMPLATABLE_VALUE(uint8_t, energy_mix)     // was EnergyMix
+  TEMPLATABLE_VALUE(uint16_t, watt)          // was ElectricPowerLevel
 
   void play(Ts... x) override {
     this->parent_->get_timer()->action_timer_activate(
-        this->start_.value(x...), this->stop_.value(x...), this->room_temperature_.value(x...),
-        this->heating_mode_.value_or(x..., HeatingMode::HEATING_MODE_OFF), this->water_temperature_.value_or(x..., 0),
-        this->energy_mix_.value_or(x..., EnergyMix::ENERGY_MIX_NONE),
-        this->watt_.value_or(x..., ElectricPowerLevel::ELECTRIC_POWER_LEVEL_0));
+        this->start_.value(x...),
+        this->stop_.value(x...),
+        this->room_temperature_.value(x...),
+        static_cast<HeatingMode>(this->heating_mode_.value_or(x..., (uint8_t) HeatingMode::HEATING_MODE_OFF)),
+        this->water_temperature_.value_or(x..., 0),
+        static_cast<EnergyMix>(this->energy_mix_.value_or(x..., (uint8_t) EnergyMix::ENERGY_MIX_NONE)),
+        static_cast<ElectricPowerLevel>(this->watt_.value_or(x..., (uint16_t) ElectricPowerLevel::ELECTRIC_POWER_LEVEL_0)));
   }
 };
 
