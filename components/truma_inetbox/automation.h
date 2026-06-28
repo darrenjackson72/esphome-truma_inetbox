2500cd5a0c3e053d2a11848c280fcd6b61346fd0
darrenjackson72 <darren@mylittleoffice.co.uk>
2026-06-28 16:43:18 +0100

    Change TEMPLATABLE_VALUE types from enums to uint8_t

--- components/truma_inetbox/automation.h
+++ components/truma_inetbox/automation.h
@@ -9,11 +9,11 @@ namespace truma_inetbox {
 template<typename... Ts> class HeaterRoomTempAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
  public:
   TEMPLATABLE_VALUE(uint8_t, temperature)
-  TEMPLATABLE_VALUE(HeatingMode, heating_mode)
+  TEMPLATABLE_VALUE(uint8_t, heating_mode)
 
   void play(const Ts &...x) override {
     this->parent_->get_heater()->action_heater_room(this->temperature_.value_or(x..., 0),
-                                                    this->heating_mode_.value_or(x..., HeatingMode::HEATING_MODE_OFF));
+                                                    (HeatingMode) this->heating_mode_.value_or(x..., (uint8_t) HeatingMode::HEATING_MODE_OFF));
   }
 };
 
@@ -28,10 +28,10 @@ template<typename... Ts> class HeaterWaterTempAction : public Action<Ts...>, pub
 
 template<typename... Ts> class HeaterWaterTempEnumAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
  public:
-  TEMPLATABLE_VALUE(TargetTemp, temperature)
+  TEMPLATABLE_VALUE(uint8_t, temperature)
 
   void play(const Ts &...x) override {
-    this->parent_->get_heater()->action_heater_water(this->temperature_.value_or(x..., TargetTemp::TARGET_TEMP_OFF));
+    this->parent_->get_heater()->action_heater_water((TargetTemp) this->temperature_.value_or(x..., (uint8_t) TargetTemp::TARGET_TEMP_OFF));
   }
 };
 
@@ -46,13 +46,13 @@ template<typename... Ts> class HeaterElecPowerLevelAction : public Action<Ts...>
 
 template<typename... Ts> class HeaterEnergyMixAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
  public:
-  TEMPLATABLE_VALUE(EnergyMix, energy_mix)
-  TEMPLATABLE_VALUE(ElectricPowerLevel, watt)
+  TEMPLATABLE_VALUE(uint8_t, energy_mix)
+  TEMPLATABLE_VALUE(uint16_t, watt)
 
   void play(const Ts &...x) override {
     this->parent_->get_heater()->action_heater_energy_mix(
-        this->energy_mix_.value_or(x..., EnergyMix::ENERGY_MIX_GAS),
-        this->watt_.value_or(x..., ElectricPowerLevel::ELECTRIC_POWER_LEVEL_0));
+        (EnergyMix) this->energy_mix_.value_or(x..., (uint8_t) EnergyMix::ENERGY_MIX_GAS),
+        (ElectricPowerLevel) this->watt_.value_or(x..., (uint16_t) ElectricPowerLevel::ELECTRIC_POWER_LEVEL_0));
   }
 };
 
@@ -67,33 +67,34 @@ template<typename... Ts> class AirconManualTempAction : public Action<Ts...>, pu
 
 template<typename... Ts> class AirconManualModeAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
  public:
-  TEMPLATABLE_VALUE(AirconMode, mode)
+  TEMPLATABLE_VALUE(uint8_t, mode)
 
   void play(const Ts &...x) override {
-    this->parent_->get_aircon_manual()->action_set_mode(this->mode_.value_or(x..., AirconMode::AIRCON_MODE_OFF));
+    this->parent_->get_aircon_manual()->action_set_mode((AirconMode) this->mode_.value_or(x..., (uint8_t) AirconMode::AIRCON_MODE_OFF));
   }
 };
 
 template<typename... Ts> class AirconManualVentModeAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
  public:
-  TEMPLATABLE_VALUE(AirconVentMode, vent_mode)
+  TEMPLATABLE_VALUE(uint8_t, vent_mode)
 
   void play(const Ts &...x) override {
     this->parent_->get_aircon_manual()->action_set_vent_mode(
-        this->vent_mode_.value_or(x..., AirconVentMode::AIRCON_VENT_LOW));
+        (AirconVentMode) this->vent_mode_.value_or(x..., (uint8_t) AirconVentMode::AIRCON_VENT_LOW));
   }
 };
 
 template<typename... Ts> class AirconManualAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
  public:
   TEMPLATABLE_VALUE(uint8_t, temperature)
-  TEMPLATABLE_VALUE(AirconMode, mode)
-  TEMPLATABLE_VALUE(AirconVentMode, vent_mode)
+  TEMPLATABLE_VALUE(uint8_t, mode)
+  TEMPLATABLE_VALUE(uint8_t, vent_mode)
 
   void play(const Ts &...x) override {
     this->parent_->get_aircon_manual()->action_aircon_manual(
-        this->temperature_.value_or(x..., 22), this->mode_.value_or(x..., AirconMode::AIRCON_MODE_OFF),
-        this->vent_mode_.value_or(x..., AirconVentMode::AIRCON_VENT_LOW));
+        this->temperature_.value_or(x..., 22),
+        (AirconMode) this->mode_.value_or(x..., (uint8_t) AirconMode::AIRCON_MODE_OFF),
+        (AirconVentMode) this->vent_mode_.value_or(x..., (uint8_t) AirconVentMode::AIRCON_VENT_LOW));
   }
 };
 
@@ -107,17 +108,18 @@ template<typename... Ts> class TimerActivateAction : public Action<Ts...>, publi
   TEMPLATABLE_VALUE(uint16_t, start)
   TEMPLATABLE_VALUE(uint16_t, stop)
   TEMPLATABLE_VALUE(uint8_t, room_temperature)
-  TEMPLATABLE_VALUE(HeatingMode, heating_mode)
+  TEMPLATABLE_VALUE(uint8_t, heating_mode)
   TEMPLATABLE_VALUE(uint8_t, water_temperature)
-  TEMPLATABLE_VALUE(EnergyMix, energy_mix)
-  TEMPLATABLE_VALUE(ElectricPowerLevel, watt)
+  TEMPLATABLE_VALUE(uint8_t, energy_mix)
+  TEMPLATABLE_VALUE(uint16_t, watt)
 
   void play(const Ts &...x) override {
     this->parent_->get_timer()->action_timer_activate(
         this->start_.value(x...), this->stop_.value(x...), this->room_temperature_.value(x...),
-        this->heating_mode_.value_or(x..., HeatingMode::HEATING_MODE_OFF), this->water_temperature_.value_or(x..., 0),
-        this->energy_mix_.value_or(x..., EnergyMix::ENERGY_MIX_NONE),
-        this->watt_.value_or(x..., ElectricPowerLevel::ELECTRIC_POWER_LEVEL_0));
+        (HeatingMode) this->heating_mode_.value_or(x..., (uint8_t) HeatingMode::HEATING_MODE_OFF),
+        this->water_temperature_.value_or(x..., 0),
+        (EnergyMix) this->energy_mix_.value_or(x..., (uint8_t) EnergyMix::ENERGY_MIX_NONE),
+        (ElectricPowerLevel) this->watt_.value_or(x..., (uint16_t) ElectricPowerLevel::ELECTRIC_POWER_LEVEL_0));
   }
 };
 
