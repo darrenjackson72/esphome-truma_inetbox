5e160a1bb4924b0981e21c33ceacb14abaa22b7d
darrenjackson72 <darren@mylittleoffice.co.uk>
2026-06-28 07:55:05 +0100

    Update framework check for ESP-IDF usage

--- components/truma_inetbox/__init__.py
+++ components/truma_inetbox/__init__.py
@@ -231,7 +231,7 @@ FINAL_VALIDATE_SCHEMA = cv.All(
 )
 
 async def to_code(config):
-    if CORE.using_esp_idf:
+    if CORE.using_framework("esp-idf"):
         # Run interrupt on core 0. ESP Home runs on core 1.
         cg.add_build_flag("-DARDUINO_SERIAL_EVENT_TASK_RUNNING_CORE=0")
         # Default Stack Size is 2048. Not enough for my operation.
